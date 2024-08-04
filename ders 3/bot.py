db592c712449500ad7ac59698a4eb0c806aa2947
heroozeroo0 <ibrahimbawersahinler@gmail.com>
2024-08-04 20:28:57 +0300

    Add files via upload

---
+++ ders 3/bot.py
@@ -0,0 +1,23 @@
+import discord
+from bot2 import gen_pass
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        await message.channel.send("hi")
+    elif message.content.startswith('!bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('!pass'):
+        await message.channel.send(gen_pass(9))
+client.run("token place")
